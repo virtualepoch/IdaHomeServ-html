2e50d680cacb1c0400bdbf361f8f8be62e0bb472
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-05-27 21:47:08 -0600

    style update

--- main.js
+++ main.js
@@ -24,77 +24,67 @@ function scroll() {
   }
 }
 
-/*
+const menuBtn = document.getElementById('menubtn')
+menuBtn.addEventListener('mousedown', function(){
+  menuBtn.style.boxShadow = 'none';
+})
+menuBtn.addEventListener('mouseup', function(){
+  menuBtn.style.boxShadow = '';
+})
 
+// DO ALL BUTTON AT SAME TIME!
+// const button = document.getElementsByClassName('button')
+// button.addEventListener('mousedown', function(){
+//   button.style.boxShadow = 'none'
+// })
+// button.addEventListener('mouseup', function(){
+//   button.style.boxShadow = ''
+// })
 
-var home = document.getElementById("home");
-var about = document.getElementById("about");
-var projects = document.getElementById("projects");
-var contact = document.getElementById("contact");
-var actionbtn = document.getElementById("actionbtn");
-var actionbtn2 = document.getElementById("actionbtn2");
-var actionbtn3 = document.getElementById("actionbtn3");
-var actionbtn4 = document.getElementById("actionbtn4");
+const aboutLink = document.getElementById('about-link')
+aboutLink.addEventListener('mousedown', function(){
+  aboutLink.style.boxShadow = 'none';
+})
+aboutLink.addEventListener('mouseup', function(){
+  aboutLink.style.boxShadow = '';
+})
 
-function changeHome() {
-  home.style.display = "block";
-  about.style.display = "none";
-  projects.style.display = "none";
-  contact.style.display = "none";
-  actionbtn.style.display = "block";
-  actionbtn2.style.display = "none";
-  actionbtn3.style.display = "none";
-  actionbtn4.style.display = "none";
-  menubox.className = "";
-  home.className = "";
-  updates.className = "";
-  testimonials.className = "";
-  gallery.className = "";
-}
-function changeAbout() {
-  home.style.display = "none";
-  about.style.display = "block";
-  projects.style.display = "none";
-  contact.style.display = "none";
-  actionbtn.style.display = "none";
-  actionbtn2.style.display = "block";
-  actionbtn3.style.display = "none";
-  actionbtn4.style.display = "none";
-  menubox.className = "";
-  home.className = "";
-  updates.className = "";
-  testimonials.className = "";
-  gallery.className = "";
-}
+const servicesLink = document.getElementById('services-link')
+servicesLink.addEventListener('mousedown', function(){
+  servicesLink.style.boxShadow = 'none';
+})
+servicesLink.addEventListener('mouseup', function(){
+  servicesLink.style.boxShadow = '';
+})
 
-function changeProjects() {
-  home.style.display = "none";
-  about.style.display = "none";
-  projects.style.display = "block";
-  contact.style.display = "none";
-  actionbtn.style.display = "none";
-  actionbtn2.style.display = "none";
-  actionbtn3.style.display = "block";
-  actionbtn4.style.display = "none";
-  menubox.className = "";
-  home.className = "";
-  updates.className = "";
-  testimonials.className = "";
-  gallery.className = "";
-}
-function changeContact() {
-  home.style.display = "none";
-  about.style.display = "none";
-  projects.style.display = "none";
-  contact.style.display = "block";
-  actionbtn.style.display = "none";
-  actionbtn2.style.display = "none";
-  actionbtn3.style.display = "none";
-  actionbtn4.style.display = "block";
-  menubox.className = "";
-  home.className = "";
-  updates.className = "";
-  testimonials.className = "";
-  gallery.className = "";
-}
-*/
+const testimonialsLink = document.getElementById('testimonials-link')
+testimonialsLink.addEventListener('mousedown', function(){
+  testimonialsLink.style.boxShadow = 'none';
+})
+testimonialsLink.addEventListener('mouseup', function(){
+  testimonialsLink.style.boxShadow = '';
+})
+
+const galleryLink = document.getElementById('gallery-link')
+galleryLink.addEventListener('mousedown', function(){
+  galleryLink.style.boxShadow = 'none';
+})
+galleryLink.addEventListener('mouseup', function(){
+  galleryLink.style.boxShadow = '';
+})
+
+const contactLink = document.getElementById('contact-link')
+contactLink.addEventListener('mousedown', function(){
+  contactLink.style.boxShadow = 'none';
+})
+contactLink.addEventListener('mouseup', function(){
+  contactLink.style.boxShadow = '';
+})
+
+const facebookLink = document.getElementById('facebook-link')
+facebookLink.addEventListener('mousedown', function(){
+  facebookLink.style.boxShadow = 'none';
+})
+facebookLink.addEventListener('mouseup', function(){
+  facebookLink.style.boxShadow = '';
+})
